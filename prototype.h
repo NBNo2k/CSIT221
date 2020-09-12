1718c40f01218e460bed0f25a687877096bb93bd
NBNo2k <52037251+NBNo2k@users.noreply.github.com>
2020-09-12 23:37:12 +0800

    Single-Subscripted Array
    Ungraded Exercise

---
+++ prototype.h
@@ -0,0 +1,19 @@
+void input(int num[], int size);
+int display(int num[], int size);
+int computeSum(int num[], int size);
+int countPos(int num[], int size);
+int countNeg(int num[], int size);
+int countEven(int num[], int size);
+int countOdd(int num[], int size);
+int findItem(int num[], int size, int item);
+int findItemAt(int num[], int size, int item);
+int findMin(int num[], int size);
+int findMax(int num[], int size);
+void add(int num[], int *count, int item);
+void newD(int num[], int count);
+void addFront(int num[], int *count, int x);
+void addAt(int num[], int *count, int item, int pos);
+void deleteEnd(int *count);
+void deleteFront(int num[], int *count);
+void deleteAt(int num[], int *count, int pos);
+
