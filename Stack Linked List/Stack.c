ad9c921f813547395f8e701e88d9b2afcb28b842
dork.kaye <52037251+NBNo2k@users.noreply.github.com>
2020-09-24 23:11:14 +0800

    Update Stack.c

--- Stack Linked List/Stack.c
+++ Stack Linked List/Stack.c
@@ -5,22 +5,6 @@
 
 Stack newStack()
 {
-	int data;Stack s;
-	
-	s = (Stack)malloc(sizeof(STACK_HEAD));
-	s->count = 0;
-	s->top = NULL;
-	
-	return;
-}
-
-Nodeptr createNode(char item)#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "Stack.h"
-
-Stack newStack()
-{
 	Stack s;
 	
 	s = (Stack)malloc(sizeof(STACK_HEAD));
@@ -114,105 +98,6 @@ void display(Stack s)
 			printf("%c ", ptr->data);
 			ptr = ptr->next;
 		}
-	}
-	printf("\n");
-}
-
-{
-	Nodeptr ptr;
-	
-	ptr = (Nodeptr)malloc(sizeof(STACK_NODE));
-	ptr->data = item;
-	ptr->next = NULL;
-	
-	return ptr;
-}
-
-void freeStack(Stack s)
-{
-	Nodeptr temp = NULL;
-	
-	while(s->top != NULL)
-	{
-		temp = s->top->next;
-		free(s->top);
-		s->top = temp;
-	}
-	free(s);
-}
-
-void push(Stack s, stackItem item)
-{               
-	Nodeptr temp = createNode(item);
-	temp->next = s->top;
-	s->top = temp;
-	s->count++;
-}
-
-int parenthesesPair(char opening, char closing)
-{
-	if(opening == '(' && closing == ')')
-		return 1;
-	else if(opening == '{' && closing == '}')
-		return 1;
-	else if(opening == '[' && closing == ']')
-		return 1;
-	else
-		return 0;
-}
-
-int balancedParentheses(stackItem parentheses[])
-{
-	Stack s = newStack(); 
-	
-	int length = strlen(parentheses);
-	int i, ans = 1;
-	
-	for(i = 0; parentheses[i] != '\0'; i++)
-	{
-		if(parentheses[i] == '(' || parentheses[i] == '[' || parentheses[i] == '{')
-			push(s, parentheses[i]);
-		else if(parentheses[i] == ')' || parentheses[i] == ']' || parentheses[i] == '}')
-		{
-			if(parenthesesPair(stackTop(s), parentheses[i]))
-				pop(s);
-			else
-				return 0;
-		}
-	}
-
-	return isEmpty(s);
-}
-
-stackItem stackTop(Stack s)
-{
-	return s->top->data;
-}
-
-void pop(Stack s)
-{             	
-	Nodeptr temp;
-	
-	temp = s->top;
-	s->top = temp->next;
-	temp->next = NULL;
-	free(temp);
-	s->count--;
-}
-
-int isEmpty(Stack s)
-{
-	return s->top == NULL;
-}
-
-void display(Stack s)
-{
-	Nodeptr ptr = s->top;
-	
-	while(ptr != NULL)
-	{
-		printf("%s ", ptr->data);
-		ptr = ptr->next;
 	}
 	printf("\n");
 }
