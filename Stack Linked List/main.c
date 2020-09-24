2846a6b2f73ebb71de987900cd237018d753b59c
dork.kaye <52037251+NBNo2k@users.noreply.github.com>
2020-09-24 23:11:40 +0800

    Update main.c

--- Stack Linked List/main.c
+++ Stack Linked List/main.c
@@ -7,15 +7,44 @@
 
 int main(int argc, char *argv[]) 
 {
-	char symbols[100];
+	stackItem parentheses[100];
+	int choice = 1, i, flag;
 	
 	Stack s = newStack();
 	
-
-	if(balancedParentheses(symbols))
-		printf("Balanced");
-	else
-		printf("Not balanced\n");
-
+	do
+	{
+		printf("Symbols: ");
+		scanf("%s", &parentheses);
+		
+		for(i = 0; parentheses[i] != '\0'; i++)
+		{
+			if(parentheses[i] == '(' || parentheses[i] == '[' || parentheses[i] == '{')
+				push(s, parentheses[i]);
+			else if(parentheses[i] == ')' || parentheses[i] == ']' || parentheses[i] == '}')
+			{
+				if(parenthesesPair(stackTop(s), parentheses[i]))
+					pop(s);
+				else
+				{
+					flag = 1;	
+					break;
+				}
+			}
+		}
+		
+		if(flag != 1 && isEmpty(s) == 1)
+			printf("Balanced\n");
+		else
+			printf("Mismatched\n");
+		
+		display(s);
+			
+		printf("Do you wish to continue(1/0)? ");
+		scanf("%d", &choice);
+		freeStack(s);
+	}
+	while(choice == 1);
+	
 	return 0;
 }
